113cecb7d383f7f2424886247ed08992de1d3a40
Mike Bass <Mike Bass>
2017-05-16 18:14:33 -0400

    idk

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,16 +12,34 @@ var db = require("../models");
 // =============================================================
 module.exports = function (app) {
   // TODO: this should come from your db.Student.find({})
-  app.get('/api/students', function (req, res) {
-    res.json([
-      {
-        first_name: 'Hello',
-        last_name: 'World'
-      }
-    ]
-  }).then(function(dbStudent) {
-      res.json(dbkcl6miep2uulocar);
+
+  	app.get("/api/students", function(req, res) {
+    db.Author.findAll({}).then(function(dbStudent) {
+      res.json(dbStudent);
     });
+  });
+
+  app.get("/api/students/:id", function(req, res) {
+     // Find one Author with the id in req.params.id and return them to the user with res.json
+    db.Student.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbStudent) {
+      res.json(dbStudent);
+    });
+  });
+
+  // app.get('/api/students', function (req, res) {
+  //   res.json([
+  //     {
+  //       first_name: 'Hello',
+  //       last_name: 'World'
+  //     }
+  //   ]
+  // }).then(function(dbStudent) {
+  //     res.json(dbkcl6miep2uulocar);
+  //   });
  
 
 app.post("/api/students", function(req, res) {

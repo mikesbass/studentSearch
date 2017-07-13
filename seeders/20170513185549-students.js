63c10fe2a530cad2c593a5bd6849aac91525b46e
Mike Bass <you@example.com>
2017-07-13 11:38:17 -0400

    herokuhater

--- seeders/20170513185549-students.js
+++ seeders/20170513185549-students.js
@@ -2,17 +2,29 @@
 
 module.exports = {
     up: function(queryInterface, Sequelize) {
-    
+        /*
+          Add altering commands here.
+          Return a promise to correctly handle asynchronicity.
+
+          Example:
+          return queryInterface.bulkInsert('Person', [{
+            name: 'John Doe',
+            isBetaMember: false
+          }], {});
+        */
         return queryInterface.bulkInsert('Student', [{
             first_name: 'John',
-            last_name: 'Doe',
-            student_email: 'abc@123.com',
-            gender: 'Male',
-            hero_name: 'Doe',
-            power: 'Doe'
+            last_name: 'Doe'
         }], {});
     },
 
     down: function(queryInterface, Sequelize) {
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
+
+          Example:
+          return queryInterface.bulkDelete('Person', null, {});
+        */
     }
 };
--- server.js
+++ server.js
@@ -7,36 +7,75 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
-
+var	methodOverride = require("method-override");
+const	jwtExp = require("express-jwt");
+const	tokenSecret = require("./tokensecret.js");
+const	cookieParser = require("cookie-parser");
 // Sets up the Express App
 // =============================================================
-var app = express();
+var apiRouter = express();
 var PORT = process.env.PORT || 8080;
 
+//cookie parser
+apiRouter.use(cookieParser(tokenSecret));
+
 // Set Handlebars as the default templating engine.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+apiRouter.engine("handlebars", exphbs({ defaultLayout: "main" }));
+apiRouter.set("view engine", "handlebars");
 
 // Requiring our models for syncing
 var db = require("./models");
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+apiRouter.use(bodyParser.json());
+apiRouter.use(bodyParser.urlencoded({ extended: true }));
+apiRouter.use(bodyParser.text());
+apiRouter.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Static directory
-app.use(express.static("./public"));
+// Override with POST having ?_method=DELETE
+apiRouter.use(methodOverride("_method"));
 
 // Routes =============================================================
 
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+var htmlRoutes = require("./routes/html-routes.js");
+var apiRoutes = require("./routes/api-routes.js");
+var authRoutes = require("./routes/auth-routes.js");
+
+// API MIDDLEWARE
+ //app.use("/api", jwtExp({ secret: tokenSecret }));
+ apiRouter.use("/api", apiRoutes);
+
+
+// AUTH MIDDLEWARE
+ apiRouter.use("/auth", authRoutes);
+
+// USER MIDDLEWARE
+ apiRouter.get("/", jwtExp({
+  secret: tokenSecret,
+   getToken: function fromCookie(req) {
+     if (req.signedCookies) {
+       return req.signedCookies.jwtAuthToken;
+     }
+     return null;
+   },
+   credentialsRequired: false
+ }), function(req, res, next) {
+   if (req.user) {
+     next();
+   } else {
+     res.redirect("/auth/login");
+   }
+ });
+
+apiRouter.use("/", htmlRoutes);
+
+apiRouter.use(express.static("./public"));
+
+
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({}).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+    apiRouter.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
 });

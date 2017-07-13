d01cc00c7d3b257ae2461fc6863e43e996581d0b
Mike Bass <you@example.com>
2017-07-13 11:00:35 -0400

    heroku old

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,32 +10,32 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-    // TODO: this should come from your db.Student.find({})
+module.exports = function (app) {
+  // TODO: this should come from your db.Student.find({})
 
     app.get("/api/students", function(req, res) {
-        db.Student.findAll({}).then(function(dbStudent) {
-            res.json(dbStudent);
-        });
+    db.Student.findAll({}).then(function(dbStudent) {
+      res.json(dbStudent);
     });
+  });
 
-    app.get("/api/students/:id", function(req, res) {
-        // Find one Student with the id in req.params.id and return them to the user with res.json
-        db.Student.findOne({
-            where: {
-                id: req.params.id
-            }
-        }).then(function(dbStudent) {
-            res.json(dbStudent);
-        });
+  app.get("/api/students/:id", function(req, res) {
+     // Find one Author with the id in req.params.id and return them to the user with res.json
+    db.Student.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbStudent) {
+      res.json(dbStudent);
     });
-
-    app.post("/api/students", function(req, res) {
-        // console.log(req.body);
-        db.Student.create(req.body).then(function(dbStudent) {
-            res.redirect("/");
-            // res.json(dbStudent);
-        });
+  });
+ 
+app.post("/api/students", function(req, res) {
+    console.log(req.body);
+    db.Student.create(req.body).then(function(dbStudent) {
+      res.redirect("/");
+      //res.json(dbStudent);
     });
+  });
 
-};
+ };

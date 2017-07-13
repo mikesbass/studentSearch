4b1877c4a237033b44ff67e43c8ab3bb5c2c521a
Mike Bass <you@example.com>
2017-07-13 19:20:00 -0400

    updated heroku because i love configs!!!

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ var env = process.env.NODE_ENV || 'development';
 var config = require('../config/config.json')[env];
 var db = {};
 
-console.log('config info', env, config)
+console.log('config info', env, config, process.env)
 if (config.use_env_variable) {
     var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
